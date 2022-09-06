3d757aebcd1bb2447ba225a79b87947dbe5cffa1
cgabrielP <cgabriel2303@gmail.com>
2022-09-06 22:17:10 +0000

    carta random

--- src/app.js
+++ src/app.js
@@ -4,8 +4,39 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+document.querySelector("#boton").addEventListener("click", changeCard);
+function changeCard() {
+  let symbol = ["♦", "♥", "♠", "♣"];
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let i = symbol.length;
+  let j = number.length;
+  let randomSymbol = symbol[Math.floor(Math.random() * i)];
+  let randomNumber = number[Math.floor(Math.random() * j)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  let colorRed = randomSymbol === "♥" || randomSymbol === "♦" ? "red" : "black";
+
+  let numbers = document.querySelector(".number");
+  let topCard = document.querySelector(".top-suit");
+  let bottomCard = document.querySelector(".bottom-suit");
+
+  topCard.style.color = colorRed;
+  bottomCard.style.color = colorRed;
+
+  topCard.innerHTML = randomSymbol;
+  bottomCard.innerHTML = randomSymbol;
+  numbers.innerHTML = randomNumber;
+}
